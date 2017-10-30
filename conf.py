53da72771937e0a9d52b1d76b7e418643713deca
Andrés Vázquez <andres@data99.com.ar>
2017-10-30 00:42:03 -0300

    renombrando build

--- conf.py
+++ conf.py
@@ -69,7 +69,7 @@ language = 'es'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['manual', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
